ae4857c395ca0c5c59f52a737fb9fd66ab1dbbf7
snshjs101 <snshjs101@gmail.com>
2018-06-27 16:28:14 +0530

    Initial commit

---
+++ app/src/main/java/com/jose/saneesh/jsonparse/DataFields.java
@@ -0,0 +1,22 @@
+package com.jose.saneesh.jsonparse;
+
+/**
+ * Created by Saneesh on 6/27/2018.
+ */
+
+class DataFields
+{
+    public String name;
+    public String description;
+    public static int position;
+    public String getUrl() {
+        return url;
+    }
+    public static int getPosition(){
+        return position;
+
+    }
+
+    public String url;
+
+}
